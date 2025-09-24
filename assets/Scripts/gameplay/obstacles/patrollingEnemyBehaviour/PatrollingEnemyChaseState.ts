777b2519b43e8727c97240145b881abf195a2fe9
Heemo2000 <hgupta01062000@gmail.com>
2025-09-24 16:54:13 +0530

    Added boilerplate for Patrolling enemy behaviour.

---
+++ assets/Scripts/gameplay/obstacles/patrollingEnemyBehaviour/PatrollingEnemyChaseState.ts
@@ -0,0 +1,24 @@
+import { Component, Node } from 'cc';
+import { IFSMState } from '../../../core/stateMachineManagement/FSMMgmt/IFSMState';
+
+export class PatrollingEnemyChaseState implements IFSMState {
+
+    onEnter(): void {
+        
+    }
+    onUpdate(deltaTime: number): void {
+        
+    }
+    onLateUpdate(deltaTime: number): void {
+        
+    }
+    onExit(): void {
+        
+    }
+    toString(): string {
+        return "PECS";
+    }
+    
+}
+
+
---
+++ assets/Scripts/gameplay/obstacles/patrollingEnemyBehaviour/PatrollingEnemyPatrolState.ts
@@ -0,0 +1,27 @@
+import { Component, Node } from 'cc';
+import { IFSMState } from '../../../core/stateMachineManagement/FSMMgmt/IFSMState';
+
+
+
+export class PatrollingEnemyPatrolState implements IFSMState {
+    
+    
+    onEnter(): void {
+        
+    }
+    onUpdate(deltaTime: number): void {
+        
+    }
+    onLateUpdate(deltaTime: number): void {
+        
+    }
+    onExit(): void {
+        
+    }
+    toString(): string {
+        return "PEPS";
+    }
+    
+}
+
+
